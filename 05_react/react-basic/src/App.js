0dcf2b937a642c04cb594835dfd1a2b19ab11766
skal48 <skal48@naver.com>
2023-10-26 19:09:14 +0900

    20231026

---
+++ 05_react/react-basic/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyFirstComponent from './component/MyFirstComponent';
+
+function App() {
+  return (
+    
+    <div className="App">
+      {/* MyFirstComponent 포함하기 */}
+      <MyFirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 05_react/react-basic/src/component/MyFirstComponent.jsx
@@ -0,0 +1,28 @@
+/* 
+  Component 
+  1. 화면 구성의 기본 단위
+  2. 함수형 컴포넌트를 주로 사용(함수형, 클래스형)  클래스 형은 아직 오류가 난다. 개발 중이라서 점차 나아진다. 
+
+*/
+
+/*
+  jsx 
+  1. 자바스크립트 확장 문법
+  2. 자바스크립트 코드 간결화 가능
+  
+*/
+function MyFirstComponent() {
+  return (
+    <div className="my-first-component">
+      <h1>MyFirstComponent</h1>
+      {/* 주석 */}
+
+    </div>
+
+
+  );
+
+  
+}
+
+export default MyFirstComponent;
